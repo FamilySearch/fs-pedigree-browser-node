5f82c25f8f483e482a05890bca66ac55e394c80d
Justin <justincyork@gmail.com>
2016-12-01 19:51:04 +0000

    begin pedigree display

--- routes/pedigree.js
+++ routes/pedigree.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var fsSession = require('../middleware/fsSession');
+var fsSession = require('../middleware/fs-session');
+var restError = require('../lib/rest-error');
 
-/* GET home page. */
 router.get('/', fsSession, function(req, res, next) {
-  res.render('pedigree');
+  var url = '/platform/tree/ancestry?generations=2&person=' + req.session.user.personId;
+  req.fs.get(url, function(error, ancestryResponse){
+    if(error || ancestryResponse.statusCode !== 200){
+      return next(error || restError(ancestryResponse));
+    }
+    var ancestry = {};
+    ancestryResponse.data.persons.forEach(function(person){
+      ancestry[person.display.ascendancyNumber] = person;
+    });
+    res.render('pedigree', {
+      ancestry: ancestry
+    });
+  });
 });
 
 module.exports = router;
