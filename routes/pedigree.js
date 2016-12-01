49fc3115df2c74b317b058bdfee1d7580c2ba80c
Justin <justincyork@gmail.com>
2016-12-01 20:46:39 +0000

    display portraits

--- routes/pedigree.js
+++ routes/pedigree.js
@@ -2,21 +2,57 @@ var express = require('express');
 var router = express.Router();
 var fsSession = require('../middleware/fs-session');
 var restError = require('../lib/rest-error');
+var async = require('async');
 
-router.get('/', fsSession, function(req, res, next) {
-  var url = '/platform/tree/ancestry?generations=2&person=' + req.session.user.personId;
-  req.fs.get(url, function(error, ancestryResponse){
-    if(error || ancestryResponse.statusCode !== 200){
-      return next(error || restError(ancestryResponse));
+function pedigreeHandler(req, res, next) {
+  var fs = req.fs,
+      personId = req.params.personId || req.session.user.personId;
+  
+  async.autoInject({
+    
+    ancestry: function(callback){
+      fs.get('/platform/tree/ancestry?generations=2&person=' + personId, function(error, response){
+        if(error || response.statusCode !== 200){
+          return callback(error || restError(response));
+        }
+        var ancestry = {};
+        response.data.persons.forEach(function(person){
+          ancestry[person.display.ascendancyNumber] = person;
+        });
+        callback(null, ancestry);
+      });
+    },
+    
+    portraits: function(ancestry, autoCallback){
+      var q = async.queue(function(person, queueCallback){
+        fs.get('/platform/tree/persons/' + person.id + '/portrait', function(error, response){
+          // don't handle errors here because missing the portrait is not fatal
+          if(response && response.headers.location){
+            person.display.portrait = response.headers.location;
+          } else {
+            console.log(error || response);
+          }
+          queueCallback();
+        });
+      });
+      q.drain = function(){
+        autoCallback();
+      };
+      for(var a in ancestry){
+        q.push(ancestry[a]);
+      }
     }
-    var ancestry = {};
-    ancestryResponse.data.persons.forEach(function(person){
-      ancestry[person.display.ascendancyNumber] = person;
-    });
-    res.render('pedigree', {
-      ancestry: ancestry
-    });
+    
+  }, function(error, results){
+    if(error){
+      next(error);
+    } else {
+      res.render('pedigree', results);
+    }
   });
-});
+}
+
+router.get('/', fsSession, pedigreeHandler);
+router.get('/:personId', fsSession, pedigreeHandler);
 
 module.exports = router;
