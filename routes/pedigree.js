4617ccba40470703c7ef698daf42c20ccc8c78ac
Justin <justincyork@gmail.com>
2016-12-01 22:01:29 +0000

    display children

--- routes/pedigree.js
+++ routes/pedigree.js
@@ -4,7 +4,12 @@ var fsSession = require('../middleware/fs-session');
 var restError = require('../lib/rest-error');
 var async = require('async');
 
-function pedigreeHandler(req, res, next) {
+
+router.get('/', fsSession, function(req, res){
+  res.redirect('/pedigree/' + req.session.user.personId);
+});
+
+router.get('/:personId', fsSession, function(req, res, next) {
   var fs = req.fs,
       personId = req.params.personId || req.session.user.personId;
   
@@ -23,7 +28,28 @@ function pedigreeHandler(req, res, next) {
       });
     },
     
-    portraits: function(ancestry, autoCallback){
+    descendancy: function(callback){
+      fs.get('/platform/tree/descendancy?person=' + personId, function(error, response){
+        if(error || response.statusCode !== 200){
+          return callback(error || restError(response));
+        }
+        var descendancy = {
+          children: []
+        };
+        response.data.persons.forEach(function(person){
+          var number = person.display.descendancyNumber;
+          if(number === '1-S'){
+            descendancy.spouse = person;
+          }
+          else if(/1\.\d+/.test(number)){
+            descendancy.children.push(person);
+          }
+        });
+        callback(null, descendancy);
+      });
+    },
+    
+    portraits: function(ancestry, descendancy, autoCallback){
       var q = async.queue(function(person, queueCallback){
         fs.get('/platform/tree/persons/' + person.id + '/portrait', function(error, response){
           // don't handle errors here because missing the portrait is not fatal
@@ -39,6 +65,10 @@ function pedigreeHandler(req, res, next) {
       for(var a in ancestry){
         q.push(ancestry[a]);
       }
+      q.push(descendancy.children);
+      if(descendancy.spouse){
+        q.push(descendancy.spouse);
+      }
     }
     
   }, function(error, results){
@@ -48,9 +78,6 @@ function pedigreeHandler(req, res, next) {
       res.render('pedigree', results);
     }
   });
-}
-
-router.get('/', fsSession, pedigreeHandler);
-router.get('/:personId', fsSession, pedigreeHandler);
+});
 
 module.exports = router;
